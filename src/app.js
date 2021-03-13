e0372210129d233905fbfc6b79c1608e7c168571
Geniorama <geniorama.info@gmail.com>
2021-03-12 20:32:42 -0500

    Ajustes faltantes

--- src/app.js
+++ src/app.js
@@ -253,13 +253,13 @@ $(function() {
     slidesToShow: 3,
     slidesToScroll: 3,
     dots: true,
+    infinite: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          infinite: true
         }
       },
       {
